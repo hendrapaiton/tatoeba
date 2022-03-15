fb008543bd2e45f0e31e8c866001096bbbf9e67a
Hendra Dwi Saputra <hendra@usa.com>
2022-03-15 19:11:42 +0700

    Menambahkan viewengine svelte pada proyek

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { svelteTemplateEngine } from './svelteTemplateEngine';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.engine('svelte', svelteTemplateEngine);
+  app.setViewEngine('svelte');
+
   await app.listen(3000);
+  Logger.log(`Server bekerja di: ${await app.getUrl()}`);
 }
 bootstrap();
---
+++ views/Index.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Layout from './Layout.svelte';
+  export let pesan;
+</script>
+
+<svelte:head>
+  <title>Index</title>
+</svelte:head>
+
+<Layout>
+  <h1>{pesan}</h1>
+</Layout>
